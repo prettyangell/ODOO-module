187cb22ee9920a7a4100f873cda0ea20929cc293
MECHAHED Nour El Islam <m.islem42@gmail.com>
2023-05-25 14:11:55 +0100

    [UPD] Refactored some parts

--- event_pfe/models/event_site.py
+++ event_pfe/models/event_site.py
@@ -1,29 +1,59 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 import base64
 import xml.etree.ElementTree as ET
-from lxml import etree
+
+STATE_DRAFT = 'draft'
+STATE_CONFIRMED = 'confirmed'
+STATES = [
+    (STATE_DRAFT, _("Draft")),
+    (STATE_CONFIRMED, _("Confirmed"))
+]
 
 
 class SiteEvent(models.Model):
     _name = "event.site"
-    _description = "handling event sites"
+    _description = _("handling event sites")
 
-    name = fields.Char(string="nom", required=True)
-    capacity = fields.Integer(string="capacité", required=True)
-    image = fields.Binary(string="image", required=True)
-    adress = fields.Char(string="adresse", required=True)
+    name = fields.Char(
+        string=_("Name"),
+        required=True
+    )
+    capacity = fields.Integer(
+        string=_("Capacity"),
+        required=True
+    )
+    image = fields.Binary(
+        string=_("Image"),
+        required=True
+    )
+    address = fields.Char(
+        string=_("Address"),
+        required=True
+    )
     section_ids = fields.One2many(
-        "section.place", "event_site_id", string="sections", required=True)
+        "section.place",
+        "event_site_id",
+        string=_("sections"),
+        required=True
+    )
     state = fields.Selection(
-        [('draft', 'Draft'), ('confirmed', 'Confirmed')], copy=False, required=True)
+        STATES,
+        default=STATE_DRAFT,
+        copy=False,
+        required=True
+    )
     event_ids = fields.One2many(
-        "event.event", "event_site_id", string="events", required=True)
+        "event.event",
+        "event_site_id",
+        string=_("events"),
+        required=True
+    )
 
     @api.constrains("image")
     def _check_image(self):
         for rec in self:
-            if rec.state == "draft":
+            if rec.state == STATE_DRAFT:
                 rec.section_ids.unlink()
                 image_data = base64.b64decode(rec.image)
                 decoded_string = image_data.decode('utf-8')
@@ -44,14 +74,14 @@ class SiteEvent(models.Model):
 
     @api.onchange("state")
     def _check_state(self):
-        if self.state == "confirmed":
+        if self.state == STATE_CONFIRMED:
             print("yes")
             self = self.with_context(readonly_fields=['image'])
 
     def action_confirm(self):
         for rec in self:
-            rec.state = "confirmed"
+            rec.state = STATE_CONFIRMED
 
-    def action_cancel(self):
+    def action_draft(self):
         for rec in self:
-            rec.state = "draft"
+            rec.state = STATE_DRAFT
