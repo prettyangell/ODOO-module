20340b4c786aea7de17718b062b19608368a5c27
yusra <yousrabouhraoua63@gmail.com>
2023-06-03 23:46:23 +0100

    thanit

--- event_pfe/static/src/js/index.js
+++ event_pfe/static/src/js/index.js
@@ -1,7 +1,7 @@
 
 
 const pathElements = document.querySelectorAll("path");
-const popup = document.querySelectorAll(".popup")[0];
+const popup = document.querySelectorAll(".popup_holder")[0];
 // popup.style.display = none
 
 function get_path_element_code(pathElement) {
@@ -16,10 +16,10 @@ function get_ticket_item_from_path_element(pathElement) {
 }
 
 
-const ticket_items = document.querySelector('.ticket_item');
+const ticket_items = document.querySelectorAll('.ticket_item');
 
 // reserved_tickets management
-const reserved_tickets = []
+let reserved_tickets = []
 function add_reservation(reservation) {
   if (!reservation.code || !reservation.dom_element) {
     throw new Error('Missing attribute in reservation !');
@@ -33,16 +33,18 @@ function remove_reservation(code) {
     throw Error('code passed is not string');
   }
   const ticket_to_remove = reserved_tickets.findIndex((value) => value.code == code)
-  ticket_to_remove.dom_element.style.display = 'none';
+  // ticket_to_remove.dom_element.style.display = 'none';
   reserved_tickets = reserved_tickets.filter((element) => element.code != code)
 }
 
 // ticket item close button management
 Array.from(ticket_items).forEach((ticket_item) => {
   const close_btn = ticket_item.querySelector('.close-btn')
+  console.log(close_btn)
   close_btn.addEventListener("click", (e) => {
-    // e.preventDefault();
-    remove_reservation(ticket_item.id);
+     e.preventDefault();
+     ticket_item.style.display="none";
+     remove_reservation(ticket_item.id);
     // state update
   })
 })
@@ -52,7 +54,7 @@ let activePopup = null;
 function showPopup() {
   alert("done");
 }
-console.log(pathElements)
+
 
 
 
@@ -89,9 +91,74 @@ active_path_element.map((pathElement) => {
 
 const registration = document.getElementById('main_registration');
 
-registration.addEventListener('click', (e) => {
+odoo.define('custom_module.custom_event', function (require) {
+  var EventRegistrationForm = require('website_event.website_event');
+  var ajax = require('web.ajax');
+var core = require('web.core');
+var Widget = require('web.Widget');
+var publicWidget = require('web.public.widget');
+
+var _t = core._t;
+  
+  EventRegistrationForm.include({
+      start: function () {
+          var self = this;
+          var res = this._super.apply(this, arguments).then(function () {
+              $('#registration_form #main_registration')
+                  .off('click')
+                  .click(function (ev) {
+                      self.on_click(ev);
+                  })
+                  .prop('disabled', false);
+          });
+          return res;
+      },
+
+      on_click: function (ev) {
+          ev.preventDefault();
+          ev.stopPropagation();
+          var $form = $(ev.currentTarget).closest('form');
+          var $button = $(ev.currentTarget).closest('[type="submit"]');
+          var post = {};
+          $('#registration_form table').siblings('.alert').remove();
+          $('#registration_form select#test').each(function () {
+              post[$(this).attr('name')] = $(this).val();
+          });
+          var tickets_ordered = _.some(_.map(post, function (value, key) { return parseInt(value); }));
+          if (!tickets_ordered) {
+              $('<div class="alert alert-info"/>')
+                  .text(_t('Please select at least one ticket.'))
+                  .insertAfter('#registration_form table');
+              return new Promise(function () {});
+          } else {
+              $button.attr('disabled', true);
+              return ajax.jsonRpc($form.attr('action'), 'call', post).then(function (modal) {
+                  var $modal = $(modal);
+                  $modal.modal({backdrop: 'static', keyboard: false});
+                  $modal.find('.modal-body > div').removeClass('container');
+                  $modal.appendTo('body').modal();
+                  $modal.on('click', '.js_goto_event', function () {
+                      $modal.modal('hide');
+                      $button.prop('disabled', false);
+                  });
+                  $modal.on('click', '.close', function () {
+                      $button.prop('disabled', false);
+                  });
+              });
+          }
+      }
+  });
+
+  return EventRegistrationForm;
+});
+
+
+
+// registration.addEventListener('click', (e) => {
 
   
-  // use reserved_tickets
-  // copy of the old registration code
-})
+
+  
+//   // use reserved_tickets
+//   // copy of the old registration code
+// })
