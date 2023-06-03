b92c1bc8e2a73b4ae2a58edfa78d155c36039a4a
Quickinline <adelbouhraoua23@gmail.com>
2023-06-03 14:06:46 +0200

    basic reprogramming

--- event_pfe/static/src/js/index.js
+++ event_pfe/static/src/js/index.js
@@ -1,42 +1,103 @@
 
 
 const pathElements = document.querySelectorAll("path");
-const popups = document.querySelectorAll(".popup");
+const popup = document.querySelectorAll(".popup")[0];
+// popup.style.display = none
+
+function get_path_element_code(pathElement) {
+  const codeAttribute = pathElement.getAttribute("code");
+  if (!codeAttribute) {
+    throw new Error('Missing code attribute! ');
+  }
+  return codeAttribute;
+}
+
+function get_ticket_item_from_path_element(pathElement) {
+  const code = get_path_element_code(pathElement);
+  const ticket_item = document.getElementById(code);
+  if (!ticket_item) {
+    throw new Error('Corresponding ticket item not found !');
+  }
+  return ticket_item
+}
+
+
+const ticket_items = document.querySelector('.ticket_item');
+
+// reserved_tickets management
+const reserved_tickets = []
+function add_reservation(reservation) {
+  if (!reservation.code || !reservation.dom_element) {
+    throw new Error('Missing attribute in reservation !');
+  }
+  reservation.dom_element.style.display = 'block';
+  reserved_tickets.push(reservation);
+
+}
+function remove_reservation(code) {
+  if (typeof code != 'string') {
+    throw Error('code passed is not string');
+  }
+  const ticket_to_remove = reserved_tickets.findIndex((value) => value.code == code)
+  ticket_to_remove.dom_element.style.display = 'none';
+  reserved_tickets = reserved_tickets.filter((element) => element.code != code)
+}
+
+// ticket item close button management
+Array.from(ticket_items).forEach((ticket_item) => {
+  const close_btn = ticket_item.querySelector('.close-btn')
+  close_btn.addEventListener("click", (e) => {
+    // e.preventDefault();
+    remove_reservation(ticket_item.id);
+    // state update
+  })
+})
+
 let activePopup = null;
 
 function showPopup() {
   alert("done");
 }
 console.log(pathElements)
-Array.from(pathElements).map((pathElement) => {
-  const codeAttribute = pathElement.getAttribute("code");
-  const popup = document.getElementById(codeAttribute);
-  if(!popup){
-    return;
-  }
+
+
+
+// we extract the valid ticket_items
+const active_path_element = Array.from(pathElements).filter((pathElement) => {
+  return !!get_ticket_item_from_path_element(pathElement)
+})
+
+
+
+
+
+active_path_element.map((pathElement) => {
+  pathElement.style.backgroundColor = 'green';
+
+  const ticket_item = get_ticket_item_from_path_element(pathElement)
 
   pathElement.addEventListener("click", () => {
 
-    if (popup) {
-      // Hide all popups
-      popups.forEach((popup) => {
-        popup.classList.remove ("show");
-      });
+    add_reservation({
+      code: get_path_element_code(pathElement),
+      dom_element: ticket_item
+    })
 
-      // Show the corresponding popup
-      popup.classList.add("show");
-      activePopup = popup;
-
-
+    if (!popup.classList.contains('show')) {
+      popup.classList.add('show')
     }
+
+    // state update
+
   });
 
-  const closeBtn = popup.querySelector(".close-btn");
-  if (closeBtn) {
-    closeBtn.addEventListener("click", (e) => {
-      e.preventDefault();
-      popup.classList.remove("show");
-      activePopup = null;
-    });
-  }
 });
+
+const registration = document.getElementById('main_registration');
+
+registration.addEventListener('click', (e) => {
+
+  
+  // use reserved_tickets
+  // copy of the old registration code
+})
